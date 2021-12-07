4c8065f82a90eeca5cf3f5f9f62bc5daba00c609
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-12-07 15:28:38 -0300

    Update Tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/react';
 
 const Tab3: React.FC = () => {
   return (
@@ -10,14 +8,30 @@ const Tab3: React.FC = () => {
           <IonTitle>Tab 3</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+    
+      <IonGrid>
+      <IonRow className="ion-align-items-center">
+        <IonCol size="auto">ion-col 00000000000000</IonCol>
+        <IonCol size="auto">ion-col 00000000000000</IonCol>
+        <IonCol size="auto">ion-col 00000000000000</IonCol>
+        <IonCol size="auto">ion-col 00000000000000</IonCol>
+      </IonRow>
+
+      <IonRow className="ion-align-items-center">
+        <IonCol size-sm>ion-col 00000000000000</IonCol>
+        <IonCol size-sm>ion-col 00000000000000</IonCol>
+        <IonCol size-sm>ion-col 00000000000000</IonCol>
+        <IonCol size-sm>ion-col 00000000000000</IonCol>
+      </IonRow>
+
+      
+
+      </IonGrid>
+
       </IonContent>
+
     </IonPage>
   );
 };
