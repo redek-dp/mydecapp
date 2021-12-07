685dfb3b4e21a4b4880b39c57d5b053fc313cc61
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-12-07 15:27:54 -0300

    Update Tab2.tsx

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,42 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>MUSIC</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+
+                <IonCard href="#" target="_blank" className="ion-align-items-center">
+
+                    <img src="https://via.placeholder.com/500x200/3b2d50/bdbac1?text=DAVIDSONBPE" width="100%" />
+
+                    <IonCardHeader>
+                        <IonCardTitle>Madison, WI</IonCardTitle>
+                    </IonCardHeader>
+
+                    <IonCardContent>
+                        This is content, without any paragraph or header tags,
+                        within an ion-cardContent element.
+                    </IonCardContent>
+
+                    <IonItem className="ion-activated">
+                        <IonIcon icon={wifi} slot="start" />
+                        <IonLabel>Card Link Item 1 activated</IonLabel>
+                    </IonItem>
+
+                </IonCard>
+
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab2;
